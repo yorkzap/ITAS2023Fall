d57f580df6e6084320fd9f42c162cbbfd4873767
Raja <118312686+yorkzap@users.noreply.github.com>
2023-09-12 13:36:55 -0700

    Add first.py dummy program

---
+++ ITAS-185/labs/raj.singh185L01/first.py
@@ -0,0 +1,9 @@
+"""
+    Course: ITAS 185 - Introduction to Programming
+    Lab 1: Printing to the screen
+    Description: This program simply prints things to the screen.
+"""
+
+print ("Information Technology & Applied Systems")
+print ("900 Fifth St, Building 180")
+print ("Nanaimo, BC, V9R 555")
