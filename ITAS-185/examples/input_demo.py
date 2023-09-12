b310bfe6343a0dd466a89412bc6cac281140291d
Raja <118312686+yorkzap@users.noreply.github.com>
2023-09-12 13:01:54 -0700

    Add script for taking input

---
+++ ITAS-185/examples/input_demo.py
@@ -0,0 +1,15 @@
+"""
+Author: Raj Singh
+Course: ITAS 185
+Date: 2023/09/12
+Description: Describe what your code does
+"""
+
+my_name = input("Enter your name ")
+my_age = input("Enter your age ")
+print(f"Hello {my_name}. You are {my_age} years old.")
+
+num3 = input("Enter the first number ")
+num4 = input("Enter the second number ")
+result = int(num3) + int(num4)
+print(f"The sum of {num3} and {num4} is {result}")
