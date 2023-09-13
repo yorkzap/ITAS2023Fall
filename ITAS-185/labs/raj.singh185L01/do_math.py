0c939a79cb97be1c41aa2a6f138193a351495c8b
Raja <118312686+yorkzap@users.noreply.github.com>
2023-09-12 23:24:38 -0700

    Improve readability

--- ITAS-185/labs/raj.singh185L01/do_math.py
+++ ITAS-185/labs/raj.singh185L01/do_math.py
@@ -4,18 +4,18 @@
     Name: Raj Singh
     Description: This program contains different Math calculations.
 """
-#Input in float datatype in order to take in decimals
+# Input in float datatype in order to take in decimals
 num1 = float(input("Enter number 1 "))
 num2 = float(input("Enter number 2 "))
 
-#Do calculations
+# Do calculations
 diff = num1 - num2
 product = num1 * num2
-quotient = num1/num2
-remainder = num1%num2
-int_division = num1//num2
+quotient = num1 / num2
+remainder = num1 % num2
+int_division = num1 // num2
 
-#Show results
+# Show results
 print(f"The difference between {num1} and {num2} is {diff}")
 print(f"The product of {num1} and {num2} is {product}")
 print(f"The quotient of {num1} and {num2} is {quotient}")
