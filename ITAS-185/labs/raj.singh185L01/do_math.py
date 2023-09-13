276fe727649344badcb4386ee689c9499e26f640
Raja <118312686+yorkzap@users.noreply.github.com>
2023-09-12 23:06:30 -0700

    Define all variables

--- ITAS-185/labs/raj.singh185L01/do_math.py
+++ ITAS-185/labs/raj.singh185L01/do_math.py
@@ -5,3 +5,10 @@
     Description: This program contains different Math calculations.
 """
 
+num1 = input("Enter number 1")
+num2 = input("Enter number 2")
+diff = num1 - num2
+product = num1 * num2
+quotient = num1/num2
+remainder = num1%num2
+int_division = num1//num2
