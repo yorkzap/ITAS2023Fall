21cbdd22aaf734f97dff2c179040a861fe8a4457
Raja <118312686+yorkzap@users.noreply.github.com>
2023-09-12 22:58:09 -0700

    Create boilerplate python script

---
+++ ITAS-185/labs/raj.singh185L01/do_math.py
@@ -0,0 +1,7 @@
+"""
+    Course: ITAS 185 - Introduction to Programming
+    Lab 1: Introduction to Python
+    Name: Raj Singh
+    Description: This program contains different Math calculations.
+"""
+
