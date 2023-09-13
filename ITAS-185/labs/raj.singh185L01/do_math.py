7950ade916bd557cc451c22d83d65977d55c4d57
Raja <118312686+yorkzap@users.noreply.github.com>
2023-09-12 23:18:52 -0700

    Write final script for lab1

--- ITAS-185/labs/raj.singh185L01/do_math.py
+++ ITAS-185/labs/raj.singh185L01/do_math.py
@@ -4,11 +4,20 @@
     Name: Raj Singh
     Description: This program contains different Math calculations.
 """
+#Input in float datatype in order to take in decimals
+num1 = float(input("Enter number 1"))
+num2 = float(input("Enter number 2"))
 
-num1 = input("Enter number 1")
-num2 = input("Enter number 2")
+#Do calculations
 diff = num1 - num2
 product = num1 * num2
 quotient = num1/num2
 remainder = num1%num2
 int_division = num1//num2
+
+#Show results
+print(f"The difference between {num1} and {num2} is {diff}")
+print(f"The product of {num1} and {num2} is {product}")
+print(f"The quotient of {num1} and {num2} is {quotient}")
+print(f"The remainder of {num1} and {num2} is {remainder}")
+print(f"The integer division of {num1} and {num2} is {int_division}")
