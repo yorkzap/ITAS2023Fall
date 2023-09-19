0296883e7b73182f6a37c9e1158c2a57a706ee6b
Raja <118312686+yorkzap@users.noreply.github.com>
2023-09-19 13:18:19 -0700

    Clean the code

--- ITAS-185/labs/raj.singh185L02/numeric_types.py
+++ ITAS-185/labs/raj.singh185L02/numeric_types.py
@@ -1,45 +1,48 @@
 """
-ITAS Lab #2, Parts #A-C
-This program demonstrates how numeric types and operators behave in Python
-##### ADD THE REST OF THE DOCSTRING HERE #####
+Course: ITAS 185 - Introduction to Programming
+Lab 2: Working with numbers and modules
+Description: This program does various arithmetic tasks
 """
+
 # Add the import statements you need here
+import random
+import math
 
-
-# TASK A- Correct these forumulae
-# Find an arithmetic average
+# TASK A
 # identifier declarations
 NUMBER = 3		# number of scores
 SCORE1 = 100	# first test score
 SCORE2 = 97 	# second test score
 SCORE3 = 90   # third test score
 
-
+# Find an arithmetic average
 average = (SCORE1 + SCORE2 + SCORE3) / NUMBER
-
 output = str(SCORE1) + " and " + str(SCORE2) + " and " + str(SCORE3) + " have an average of " + str(average)
 print(output)
-
-# Convert Fahrenheit temperatures to Celsius
 BOILING_IN_F = 212      # boiling temperature
 
+# Convert Fahrenheit temperatures to Celsius
 f_to_c = 5/9 * BOILING_IN_F - 32
-
 print(f"{BOILING_IN_F} in Fahrenheit is {f_to_c} in Celsius.", end="\n\n")
 
+# TASK B
+# Prompt the user to enter their name and read it in to a variable called first_name
+first_name = input('Enter your first name: ')
+# Prompt the user to enter the lowest number that their lucky number could be and call the variable low
+low = int(input('Enter the lowest number your lucky number can be: '))
+# Prompt the user to enter the highest number that their lucky number could be and call the variable high
+high = int(input('Enter the highest number your lucky number can be: '))
+# Generate a random integer between low and high and call it lucky_number using the randint function
+lucky_number = random.randint(low, high)
+# Print out a message to the user including their name and lucky number 
+print(f"Hello {first_name}. Your lucky number is {lucky_number}")
 
-# ADD LINES FOR TASK B HERE
-# Note you also need to import the random module where indicated above
-# Next:
-#    prompt the user to enter their name and read it in to a variable called first_name
-#    prompt the user to enter the lowest number that their lucky number could be and call the variable low
-#    prompt the user to enter the highest number that their lucky number could be and call the variable high
-#    generate a random integer between low and high and call it lucky_number using the randint function
-#    print out a message to the user including their name and lucky number 
-
-# ADD LINES FOR TASK C HERE
-# prompt the user for a diameter of a sphere
-# read the diameter
-# calculate the radius
-# calculate the volume 
-# print out the volume
+# TASK C
+# Prompt the user for a diameter of a sphere
+diameter = float(input("Enter the diameter "))
+# Calculate the radius
+radius = diameter / 2
+# Calculate the volume 
+approximate_volume = (4/3) * math.pi * radius ** 3
+# Print out the volume
+print(approximate_volume)
