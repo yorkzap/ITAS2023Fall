6779f83b85ba3cbe86e982b1ff17dbd2e276f8b5
Raja <118312686+yorkzap@users.noreply.github.com>
2023-09-19 13:49:24 -0700

    Add operators exercise solution

---
+++ ITAS-185/labs/raj.singh185L02/operators.py
@@ -0,0 +1,32 @@
+"""
+Course: ITAS 185 - Introduction to Programming
+Lab 2: Working with numbers and modules
+Description: This program does various calculations with operators
+"""
+
+import math
+
+total = 0
+print(total)
+
+total += 96
+print(f"total is {total}")
+
+total -= 8
+print(f"total is {total}")
+
+total /= 3
+total = int(total)
+print(f"total is {total}")
+
+total %= 5
+print(f"total is {total}")
+
+total **= 3
+print(f"total is {total}")
+
+total /= 8
+print(f"total is {total}")
+
+total *= 7
+print(f"total is {total}")
