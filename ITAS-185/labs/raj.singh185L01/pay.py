ae1b3bc7cc96bb2d4fe382fc81e3d86dec0d448a
Raja <118312686+yorkzap@users.noreply.github.com>
2023-09-19 12:00:48 -0700

    Adjust code to abide by PEP 8

--- ITAS-185/labs/raj.singh185L01/pay.py
+++ ITAS-185/labs/raj.singh185L01/pay.py
@@ -7,9 +7,9 @@
 
 wage = input("Enter the wages per hour ")
 hours = input("Enter the number of hours worked ")
-gross_pay = float(wage)*float(hours)
-after_tax = gross_pay*0.77 #Take away 23% taxes from the gross pay
-add_bonus = after_tax*1.13 #Add 13% bonus to after_tax
+gross_pay = float(wage) * float(hours)
+after_tax = gross_pay * 0.77 #Take away 23% taxes from the gross pay
+add_bonus = after_tax * 1.13 #Add 13% bonus to after_tax
 print(f"Gross pay: $ {gross_pay:.2f}")
 print(f"After tax: $ {after_tax:.2f}")
 print(f"Total pay: $ {add_bonus:.2f}") #Print the final amount
