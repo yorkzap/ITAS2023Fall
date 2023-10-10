b7f3513b5c4ae2965d9c9979e422343fc6de4e36
Raja <118312686+yorkzap@users.noreply.github.com>
2023-10-10 10:41:22 -0700

    Do 185 week 5 example 3

---
+++ ITAS-185/examples/eg01_string.py
@@ -0,0 +1,8 @@
+my_var = "My name is Marvin"
+print(f'''
+This is a multi-line string
+    The spaces and everything
+  get counted.
+        and added when it is printed
+      even the {my_var}
+''')
