4e77ade832275714da49cb0dfefacae271c0f329
Raja <118312686+yorkzap@users.noreply.github.com>
2023-10-17 23:09:52 -0700

    Fix docstring

--- ITAS-185/labs/raj.singh185L05/assets/part_b/do_sum.py
+++ ITAS-185/labs/raj.singh185L05/assets/part_b/do_sum.py
@@ -1,8 +1,9 @@
 """
 Course: ITAS 185 - Introduction to Programming
-Lab05: Part B (Program to Create Sequence Datatypes using List Comprehension)
+Lab05: Part B - Do Sum
 Description:
-    The program declares various sequence datatypes using list comprehension and then displays the results using print statements.
+    This program calculates the sum of columns in a text file. It prompts the user for a filename, reads the file, 
+    and then calculates the sum of each column, returning a list of the sums.
 """
 
 # Prompt the user for a filename
@@ -12,11 +13,14 @@ def sum_column(filename):
     file = open(filename, "r")
     file_list = file.readlines()
     file.close()
+
     # Splitting lines to columns
     list_inside_list = [line.split() for line in file_list]
+
     # Create a list of the sum of the columns
     sum_list = [sum([int(line[i]) for line in list_inside_list]) for i in range(len(list_inside_list[0]))]
-    # Return the list of sums
+
+    # Return the list of the sum of the columns
     return sum_list
 
 print(sum_column(filename))
