0808a268d309ea05b4a4bb65374a4398f4381b7d
Raja <118312686+yorkzap@users.noreply.github.com>
2023-10-17 23:21:16 -0700

    Open and read lines for counting letters

---
+++ ITAS-185/labs/raj.singh185L05/assets/part_b/count_letters.py
@@ -0,0 +1,12 @@
+"""
+Course: ITAS 185 - Introduction to Programming
+Lab05: Part B - Count Letters
+Description:
+    XXXXXXXXXX
+"""
+
+file = open("count_letters.txt", "r")
+
+file_list = file.readlines()
+
+print(file_list)
