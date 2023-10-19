39cb7a7c1b8888a9abd5950dff2497ed9d61213c
Raja <118312686+yorkzap@users.noreply.github.com>
2023-10-19 09:37:48 -0700

    Add 185 assignment1 docstring

---
+++ ITAS-185/assignments/raj.singh_185A01_First/MovieGenre.py
@@ -0,0 +1,7 @@
+"""
+Course: ITAS 185 - Introduction to Programming
+Assignment 01: Movie Genre Program
+Description: A program that allows users to manage a movie list where they can add, update,
+             remove, search, and display movies along with their genres. The program utilizes
+             a dictionary where the movie's name is the key and the genre is the value.
+"""
