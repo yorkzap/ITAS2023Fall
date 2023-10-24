e5040cc0a3c3bb74042277a26baf1215d4ee196c
Raja <118312686+yorkzap@users.noreply.github.com>
2023-10-24 11:52:28 -0700

    Add run program for class Car

---
+++ ITAS-185/labs/raj.singh185L06/car_run.py
@@ -0,0 +1,6 @@
+"""
+Course: ITAS 185 - Introduction to Programming
+Lab06: Importing Classes with constructors
+Description:
+    This program imports the Employee class, calls it's methods and display car information
+"""
