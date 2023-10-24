85c00393caeb8829eee3fb330460286b58265ce4
Raja <118312686+yorkzap@users.noreply.github.com>
2023-10-24 11:06:27 -0700

    Create class Employee without constructors

---
+++ ITAS-185/labs/raj.singh185L06/Employee.py
@@ -0,0 +1,15 @@
+"""
+Course: ITAS 185 - Introduction to Programming
+Lab06: Employee Class
+Description:
+    The Employee class defines attributes and a method for managing employee information.
+"""
+
+class Employee:
+    emp_name = ""
+    id_number = 0
+    department = ""
+    position = ""
+
+    def get_info(self):
+        return f"Name: {self.emp_name}\tID: {self.id_number}\tDepartment: {self.department}\tPosition: {self.position}"
