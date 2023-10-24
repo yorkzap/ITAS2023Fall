5fa4e699003809db13c7bba1157d68ba4534ddeb
Raja <118312686+yorkzap@users.noreply.github.com>
2023-10-24 12:56:13 -0700

    Remove further prints after speed reaches 0

--- ITAS-185/labs/raj.singh185L06/car_run.py
+++ ITAS-185/labs/raj.singh185L06/car_run.py
@@ -4,3 +4,19 @@ Lab06: Importing Classes with constructors
 Description:
     This program imports the Employee class, calls it's methods and display car information
 """
+
+import Car as car
+
+# Create a Car object
+my_car = car.Car(2023, "Ford", 0)
+
+# Accelerate the car 5 times
+for i in range(5):
+    my_car.accelerate()
+    print(my_car)
+
+# Brake the car 6 times and display information after each brake
+for i in range(6):
+    if i > 0:
+        my_car.brake()
+        print(my_car)
