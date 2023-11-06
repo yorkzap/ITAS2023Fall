f4322903cc3ea359ad7550f199cb04bbcb1ef208
Raja <118312686+yorkzap@users.noreply.github.com>
2023-11-05 23:33:12 -0800

    Complete 181 Assignment 02 test file

--- ITAS-181/assignments/assignment02/ITAS181_Assignment02_Raj.Singh.sh
+++ ITAS-181/assignments/assignment02/ITAS181_Assignment02_Raj.Singh.sh
@@ -14,8 +14,8 @@ do
     username="${firstname:0:1}${lastname,,}"
 
     # Capitalize the first letter of first and last name, and then join them with a comma and space
-    proper_lastname=$(echo "${lastname^}")
-    proper_firstname=$(echo "${firstname^}")
+    proper_lastname="${lastname^}"
+    proper_firstname="${firstname^}"
     full_name="$proper_lastname, $proper_firstname"
 
     # Determine the group based on the year
@@ -37,6 +37,14 @@ do
         # Create the user with the home directory, set the user to the correct group,
         # and add the full name in the 'Lastname, Firstname' format to the comment field
         sudo useradd -m -u "$userid" -c "$full_name" -d "/home/$username" -g "$groupname" "$username"
+        # Set directory permissions to 700
+        sudo chmod 700 "/home/$username"
+        # Format DOB from mm/dd/yy to yymmdd
+        IFS='/' read -r mm dd yy <<< "$dob"
+        password="${firstname:0:1}${lastname:0:1}$yy$mm$dd"
+        # Change password to the correct format
+        echo "$username:$password" | sudo chpasswd
+        echo "User created: $username with password: $password"
     fi
 
 done < "$csv_file"
