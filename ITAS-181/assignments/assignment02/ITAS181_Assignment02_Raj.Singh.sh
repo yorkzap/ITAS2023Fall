244999067a057458f6d26020981b1c3d1730712f
Raja <118312686+yorkzap@users.noreply.github.com>
2023-11-06 00:18:26 -0800

    Add printing statements

--- ITAS-181/assignments/assignment02/ITAS181_Assignment02_Raj.Singh.sh
+++ ITAS-181/assignments/assignment02/ITAS181_Assignment02_Raj.Singh.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Script to create users from a CSV file
+
 # Create a variable for the CSV file
 csv_file="./data.csv"
 
@@ -7,11 +9,23 @@ csv_file="./data.csv"
 year1_uid_start=1000
 year2_uid_start=2000
 
+# Ensure the groups exist
+echo "Checking if groups exist..."
+sudo getent group ITAS_YR1 || sudo groupadd ITAS_YR1
+sudo getent group ITAS_YR2 || sudo groupadd ITAS_YR2
+echo "Groups checked."
+
+# User creation iteration counter
+user_count=1
+
 # Reading the CSV and creating users with the full name in the comment field
 while IFS=',' read -r firstname lastname dob year
 do
+    echo "Processing user #${user_count}..."
+
     # Extract the username from the first name and last name, lowercase it
-    username="${firstname:0:1}${lastname,,}"
+    username="${firstname:0:1}${lastname}"
+    username="${username,,}"
 
     # Capitalize the first letter of first and last name, and then join them with a comma and space
     proper_lastname="${lastname^}"
@@ -36,15 +50,42 @@ do
     if [ ! -z "$groupname" ]; then
         # Create the user with the home directory, set the user to the correct group,
         # and add the full name in the 'Lastname, Firstname' format to the comment field
-        sudo useradd -m -u "$userid" -c "$full_name" -d "/home/$username" -g "$groupname" "$username"
-        # Set directory permissions to 700
-        sudo chmod 700 "/home/$username"
-        # Format DOB from mm/dd/yy to yymmdd
-        IFS='/' read -r mm dd yy <<< "$dob"
-        password="${firstname:0:1}${lastname:0:1}$yy$mm$dd"
-        # Change password to the correct format
-        echo "$username:$password" | sudo chpasswd
-        echo "User created: $username with password: $password"
+        echo "Creating user $username..."
+        sudo useradd -m -u "$userid" -c "$full_name" -d "/home/$username" -g "$groupname" -s "/bin/bash" "$username"
+        if [ $? -eq 0 ]; then
+            # Set directory permissions to 700
+            echo "Setting directory permissions for /home/$username..."
+            sudo chmod 700 "/home/$username"
+            
+            # Format DOB from mm/dd/yy to yymmdd and initials to lowercase for the password
+            IFS='/' read -r mm dd yy <<< "$dob"
+            # Extract the first letter of the first name, lowercase it
+            first_initial="${firstname,,}"
+            first_initial="${first_initial:0:1}"
+            
+            # Extract the first letter of the last name, lowercase it
+            last_initial="${lastname,,}"
+            last_initial="${last_initial:0:1}"
+
+            # Combine them with the date of birth
+            password="${first_initial}${last_initial}$yy$mm$dd"
+            
+            # Change password to the correct format
+            echo "Updating password for $username..."
+            echo "$username:$password" | sudo chpasswd
+            echo "User created: $username with password: $password"
+        else
+            echo "Failed to create user: $username"
+        fi
+    else
+        echo "Groupname for $username is empty, skipping user creation."
     fi
 
+    # Increment the user creation counter
+    ((user_count++))
+
+    echo "User #${user_count} processed."
+
 done < "$csv_file"
+
+echo "Script completed. Processed $user_count users."
