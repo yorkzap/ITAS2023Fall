95747d57d86e4edbbd736f2ce89e93a23e45dce1
Raja <118312686+yorkzap@users.noreply.github.com>
2023-11-10 13:21:10 -0800

    Comply with PEP 8

--- ITAS-185/labs/raj.singh185L07/Vehicle.py
+++ ITAS-185/labs/raj.singh185L07/Vehicle.py
@@ -2,98 +2,107 @@
 Course: ITAS 185 - Introduction to Programming
 Lab06: Vehicle Class
 Description:
-    EDITTHIS_EDITTHIS_EDITTHIS_EDITTHIS_EDITTHIS_EDITTHIS_
+    This module defines a hierarchy of classes that model different types of vehicles.
+    It includes a base Vehicle class along with derived classes Car, Transport,
+    Pickup, and SportsCar, each with specific attributes and methods.
 """
 
 class Vehicle:
+    """
+    A class representing a generic vehicle.
+    """
+
     def __init__(self, color, manufacturer, speed, top_speed, year):
         self.color = color
         self.manufacturer = manufacturer
-        self.speed = float(0.0)
+        self.speed = 0.0  # Speed is initialized to 0.0
         self.top_speed = float(top_speed)
         self.year = int(year)
         
     def accelerate(self, amount=3):
-         """
-         Increases the speed by the amount passed default 3
-         """
-         self.speed = self.speed + amount
-         self.speed = self.top_speed if self.speed > self.top_speed else self.speed
+        """
+        Increase the vehicle's speed by a specified amount, up to the top_speed.
+        """
+        self.speed += amount
+        if self.speed > self.top_speed:
+            self.speed = self.top_speed
     
     def brake(self, amount=2):
         """
-        Decreases the speed by the amount passed or default 2
+        Decrease the vehicle's speed by a specified amount, not going below 0.
         """
-        self.speed = self.speed - amount
-        self.speed = 0 if self.speed < 0 else self.speed
+        self.speed -= amount
+        if self.speed < 0:
+            self.speed = 0
         
     def __str__(self):
-        return f"{self.year} {self.color} {self.manufacturer} is travelling {self.speed} kph with a maximum speed of {self.top_speed} kph"
+        return f"{self.year} {self.color} {self.manufacturer} is travelling at {self.speed} kph with a top speed of {self.top_speed} kph."
     
     def __eq__(self, other):
-        return True if self.top_speed == other.top_speed else False
+        return self.top_speed == other.top_speed
     
 class Car(Vehicle):
+    """
+    A class representing a car, which is a type of vehicle.
+    """
+
     def __init__(self, color, manufacturer, speed, top_speed, year, number_of_doors, is_electric):
-        super().__init__(color, manufacturer, speed, top_speed, year) # Referring to the constructor of the Vehicle class
+        super().__init__(color, manufacturer, speed, top_speed, year)
         self.number_of_doors = int(number_of_doors)
         self.is_electric = bool(is_electric)
         
-    def accelerate(self):
-        super().accelerate(2)
+    def accelerate(self, amount=2):
+        """
+        Accelerate the car by a fixed amount of 2.
+        """
+        super().accelerate(amount)
         
     def __str__(self):
-        initial_string = super().__str__()
-        if self.is_electric:
-            return f"{initial_string}, It has {self.number_of_doors} doors and is electric"
-        else:
-            return f"{initial_string}, It has {self.number_of_doors} doors and is not electric"
+        electric_status = 'is electric' if self.is_electric else 'is not electric'
+        return f"{super().__str__()}, It has {self.number_of_doors} doors and {electric_status}."
         
-"""
-9.	Create a class called Transport which extends Vehicle. It has the extra attribute of: 
-payload: float
-Method	Purpose
-Constructor	A constructor that accepts all attributes and set the values accordingly by first calling the super method and then assigning the extra attribute
-accelerate	Has NO parameters. Overrides the accelerate method of the Vehicle class and calls the Vehicle class accelerate method with the value 1
-toString()	Calls the super method to get the initial string and adds: 
-It can carry a payload of payload tons to end of the string. For example,
-1999 brown Mac is travelling 100 kph with a maximum speed of 160 kph. It can carry a payload of 300 tons.
-"""
-
 class Transport(Vehicle):
+    """
+    A class representing a transport vehicle, capable of carrying a payload.
+    """
+
     def __init__(self, color, manufacturer, speed, top_speed, year, payload):
         super().__init__(color, manufacturer, speed, top_speed, year)
         self.payload = float(payload)
         
     def accelerate(self):
+        """
+        Accelerate the transport by a fixed amount of 1.
+        """
         super().accelerate(1)
         
     def __str__(self):
-        initial_string = super().__str__()
-        return f"{initial_string}. It can carry a payload of {self.payload} tons"
+        return f"{super().__str__()}, It can carry a payload of {self.payload} tons."
     
-"""10.	Create a class called Pickup which extends Car. 
-Method	Purpose
-Constructors	One constructor that accepts all attributes and set the values accordingly by calling the super method.
-accelerate	Has no parameters and calls the accelerate method of the Car class with the value 1.5
-"""
-
 class Pickup(Car):
+    """
+    A class representing a pickup truck, which is a specialized type of car.
+    """
+
     def __init__(self, color, manufacturer, speed, top_speed, year, number_of_doors, is_electric):
         super().__init__(color, manufacturer, speed, top_speed, year, number_of_doors, is_electric)
         
     def accelerate(self):
+        """
+        Accelerate the pickup by a fixed amount of 1.5.
+        """
         super().accelerate(1.5)
 
-"""11.	Create a class called SportsCar which extends Car.
-Method	Purpose
-Constructor	One constructor that accepts all attributes and set the values accordingly by calling the super method.
-accelerate	Has no parameters and calls the accelerate method of the Car class with the value 4
-"""
-
 class SportsCar(Car):
+    """
+    A class representing a sports car, which is a specialized type of car.
+    """
+
     def __init__(self, color, manufacturer, speed, top_speed, year, number_of_doors, is_electric):
         super().__init__(color, manufacturer, speed, top_speed, year, number_of_doors, is_electric)
         
     def accelerate(self):
+        """
+        Accelerate the sports car by a fixed amount of 4.
+        """
         super().accelerate(4)
