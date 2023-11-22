58a11f4675bc6ce9c471ef9301425044c056a30b
Raja <118312686+yorkzap@users.noreply.github.com>
2023-11-22 08:01:54 -0800

    Write docstring for 185 assignment02 python script

---
+++ ITAS-185/assignments/raj.singh_185A02_Second/start.py
@@ -0,0 +1,13 @@
+"""
+Course: ITAS 185 - Introduction to Programming
+Assignment 02: Wordle Game
+Description:
+    This WordleGame class simulates the popular word guessing game 'Wordle'. The 
+    game selects a secret five-letter word which the player must guess within a 
+    limited number of attempts. Each guess provides feedback to the player in the 
+    form of colored letters: green for correct letters in the correct position, 
+    yellow for correct letters in the wrong position, and uncolored for incorrect 
+    letters. The game continues until the player either guesses the word correctly 
+    or runs out of attempts, at which point the game can be restarted.
+"""
+
